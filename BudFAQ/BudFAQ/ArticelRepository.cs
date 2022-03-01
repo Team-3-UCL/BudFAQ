f1a54521ca99787c3bb27deebbd317602eec849d
Mikkel Fuglsang <mikkelfuglsang20@gmail.com>
2022-03-01 10:45:26 +0100

    lavet repo
    Lavet Article class og Constructor i ArticleRepo

--- BudFAQ/BudFAQ/ArticelRepository.cs
+++ BudFAQ/BudFAQ/ArticelRepository.cs
@@ -38,9 +38,42 @@ namespace BudFAQ
             }
         }
 
+        private List<Article> _articles;
+        string connectionString = "Server = 10.56.8.36; Database=P1DB03;User Id = P1-03; Password=OPENDB_03;";
+        public ArticelRepository()
+        {
+            _articles = new List<Article>();
+
+
+            using (SqlConnection connection = new(connectionString))
+            {
+                string sCmd = "SELECT * FROM dbo.Artikel";
+                SqlCommand cmd = new SqlCommand(sCmd, connection);
+                connection.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {              
+                        Article article = new();
+                        article.Name = reader["ArticleName"].ToString();
+                        article.Text = reader["ArticleText"].ToString();
+                        article.Link = reader["ArticleLink"].ToString();
+
+                        _articles.Add(article);
+
+                    }
+                }
+
+
+            }
+        }
 
 
 
 
     }
 }
+
+
+
+
---
+++ BudFAQ/BudFAQ/Article.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudFAQ
+{
+    public class Article
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        private string link;
+
+        public string Link
+        {
+            get { return link; }
+            set { link = value; }
+        }
+
+
+
+
+
+
+    }
+}
